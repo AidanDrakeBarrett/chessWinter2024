b48d3bc783021f0c9837b0a95a557b546cd0e75c
AidanDrakeBarrett <ab2576@byu.edu>
2024-03-18 00:22:28 -0600

    Phase 3. Commit 19. Just need to write API tests

--- server/src/test/java/serviceTests/GamesServiceTest.java
+++ server/src/test/java/serviceTests/GamesServiceTest.java
@@ -1,13 +1,33 @@
 package serviceTests;
 
+import dataAccess.AbbreviatedGameData;
+import dataAccess.MemoryAuthDAO;
+import dataAccess.MemoryGameDAO;
 import org.junit.jupiter.api.Test;
+import server.ResponseException;
+import service.GamesService;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GamesServiceTest {
+    private GamesService service = new GamesService();
+    private MemoryGameDAO gameDAO = new MemoryGameDAO();
+    private MemoryAuthDAO authDAO = new MemoryAuthDAO();
 
     @Test
-    void listGames() {
+    void listGames() throws ResponseException {//FIXME: LESDOTHIZZZ
+        String gameName = "game";
+        int id = gameDAO.createGame(gameName);
+        AbbreviatedGameData abbreviatedGameData = new AbbreviatedGameData(id, null, null, gameName);
+        ArrayList<AbbreviatedGameData> abbreviations = new ArrayList<>();
+        abbreviations.add(abbreviatedGameData);
+
+        String username = "username";
+        String authToken = authDAO.createAuth(username).username();
+        assertEquals(abbreviations, service.listGames(authToken));
     }
 
     @Test
