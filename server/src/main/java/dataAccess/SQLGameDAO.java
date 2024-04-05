005e19dda0028769f8fb4e2fcfcf7007c750a276
AidanDrakeBarrett <ab2576@byu.edu>
2024-04-04 22:41:37 -0600

    Phase 4. Commit 18. Deleted some redundant lines

--- server/src/main/java/dataAccess/SQLGameDAO.java
+++ server/src/main/java/dataAccess/SQLGameDAO.java
@@ -11,8 +11,6 @@ import java.sql.*;
 import java.util.HashSet;
 import java.util.Objects;
 
-import static java.sql.Statement.RETURN_GENERATED_KEYS;
-import static java.sql.Types.NULL;
 public class SQLGameDAO implements GameDAO{
     public SQLGameDAO() {
         try {
